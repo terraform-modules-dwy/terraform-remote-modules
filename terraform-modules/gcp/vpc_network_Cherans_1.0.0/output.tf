f5f7cb3fc624a6e1faf29241e6a96647217c708f
DevOpsWithYoge <156210181+AllAboutAzure@users.noreply.github.com>
2024-03-27 19:57:45 +0530

    Delete terraform-modules/gcp/vpc_network_Cherans_1.0.0 directory

--- terraform-modules/gcp/vpc_network_Cherans_1.0.0/output.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  value = google_compute_network.vpc_network.name
-  description = "name of the gcp vpc network"
-}
-
-output "id" {
-  value = google_compute_network.vpc_network.id
-  description = "id of the gcp network"
-}
